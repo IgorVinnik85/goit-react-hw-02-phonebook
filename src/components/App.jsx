6b599b9037f13f0318d7bcaaf3bdd29d31281f13
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-05 15:37:37 +0300

    add_json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import { FindContacts } from './FindContacts/FindContacts';
 import { FormPhonebook } from './FormPhonebook/FormPhonebook';
 import { ContactsPhonebook } from './ContactsPhonebook/ContactsPhonebook';
+import StaticContact from '../components/ContactsPhonebook/StaticContact.json';
 import { nanoid } from 'nanoid';
 
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: StaticContact,
     filter: '',
   };
 
